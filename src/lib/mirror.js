7623ce067a517822182f7cb745a7e4d4a4c0be20
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-11-02 19:56:01 +0100

    fixed path can't be a Function

--- src/lib/mirror.js
+++ src/lib/mirror.js
@@ -1,6 +1,9 @@
+import path from 'path'
 import glob from 'glob-all'
 
 import { copy, deleteExtra } from './fs'
+
+const cwd = process.cwd()
 
 const mirror = (source, target, options, notify) => {
   let flag = true
@@ -9,9 +12,11 @@ const mirror = (source, target, options, notify) => {
   deleteExtra(target, options, notify)
 
   for (let i = 0, l = files.length; i < l; ++i) {
-    if (!copy(files[i], target, notify)) {
+    const file = files[i]
+
+    if (!copy(file, path.join(target, path.relative(cwd, file)), notify)) {
       flag = false
-      break;
+      break
     }
   }
 
