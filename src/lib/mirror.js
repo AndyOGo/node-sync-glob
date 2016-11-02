3a23b44dda58d5c20161c1dade0abdb933169cce
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-11-02 19:43:32 +0100

    fixed mirro not returning bool

--- src/lib/mirror.js
+++ src/lib/mirror.js
@@ -3,13 +3,19 @@ import glob from 'glob-all'
 import { copy, deleteExtra } from './fs'
 
 const mirror = (source, target, options, notify) => {
+  let flag = true
   const files = glob.sync(source)
 
   deleteExtra(target, options, notify)
 
-  files.forEach((file) => {
-    copy(file, target, notify)
-  })
+  for (let i = 0, l = files.length; i < l; ++i) {
+    if (!copy(files[i], target, notify)) {
+      flag = false
+      break;
+    }
+  }
+
+  return flag
 }
 
 export default mirror
