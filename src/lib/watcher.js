0e3ae389c7a2685802734a2a6794d1b37e7a1ca6
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-11-02 19:14:20 +0100

    clarified was f is

--- src/lib/watcher.js
+++ src/lib/watcher.js
@@ -2,12 +2,12 @@ import path from 'path'
 
 import { copy, deleteExtra } from './fs'
 
-export const watcherCopy = (source, target, options, notify) => (f, stats) => {
-  copy(f, path.join(target, path.relative(source, f)), notify)
+export const watcherCopy = (source, target, options, notify) => (file, stats) => {
+  copy(file, path.join(target, path.relative(source, file)), notify)
 }
 
-export const watcherDestroy = (source, target, options, notify) => (f) => {
-  deleteExtra(path.join(target, path.relative(source, f)), options, notify)
+export const watcherDestroy = (source, target, options, notify) => (file) => {
+  deleteExtra(path.join(target, path.relative(source, file)), options, notify)
 }
 
 export const watcherError = (options, notify) => (err) => {
