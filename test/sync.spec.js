0f695ac348510c55dc9caebf88cf438ee44f2e0d
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-02-01 19:40:50 +0100

    refactore object

--- test/sync.spec.js
+++ test/sync.spec.js
@@ -3,11 +3,13 @@ import fs from 'fs-extra'
 import syncGlob from '../src/index'
 import { compare, compareDir } from './helpers'
 
+const watch = true
+
 describe('node-sync-glob', () => {
   it('should sync a file', (done) => {
     let hasChanged = false
 
-    const close = syncGlob('test/mock/a.txt', 'tmp', { watch: true }, compare(() => {
+    const close = syncGlob('test/mock/a.txt', 'tmp', { watch }, compare(() => {
       if (!hasChanged) {
         hasChanged = true
         fs.appendFileSync('test/mock/a.txt', 'foobarbaz')
