6ba67c73b1b9569d89d39c3ad9d38104476877f5
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-02-25 19:56:16 +0100

    fixed default params

--- src/lib/promisify.js
+++ src/lib/promisify.js
@@ -1,4 +1,4 @@
-const promisify = (func, { context = null, multiArgs }) => (...args) => new Promise((resolve, reject) => {
+const promisify = (func, { context = null, multiArgs } = {}) => (...args) => new Promise((resolve, reject) => {
   args.push((err, ...values) => {
     if (err) reject(err)
     else resolve(multiArgs ? values : values[0])
