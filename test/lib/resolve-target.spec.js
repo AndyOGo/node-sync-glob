d48ce51b0dcc55ab7988a0a37ac91d50f7bfbf74
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-02-28 02:23:17 +0100

    added resolve target spec

---
+++ test/lib/resolve-target.spec.js
@@ -0,0 +1,26 @@
+import resolveTarget from '../../src/lib/resolve-target'
+
+describe('lib/resolve-target', () => {
+  it('should resolve targets', () => {
+    const bases = [
+      'test',
+      'test/mock',
+      'test/mock/foo',
+      'test/mock/bar',
+      'test/mock/@org',
+    ]
+    const resolveTargetFromBases = resolveTarget(bases)
+    const target = 'dist'
+
+    expect(resolveTargetFromBases('test/a.txt', target)).toEqual('dist/a.txt')
+    expect(resolveTargetFromBases('test/lib/a.txt', target)).toEqual('dist/lib/a.txt')
+    expect(resolveTargetFromBases('test/mock/a.txt', target)).toEqual('dist/a.txt')
+    expect(resolveTargetFromBases('test/mock/lib/a.txt', target)).toEqual('dist/lib/a.txt')
+    expect(resolveTargetFromBases('test/mock/foo/a.txt', target)).toEqual('dist/a.txt')
+    expect(resolveTargetFromBases('test/mock/foo/lib/a.txt', target)).toEqual('dist/lib/a.txt')
+    expect(resolveTargetFromBases('test/mock/bar/a.txt', target)).toEqual('dist/a.txt')
+    expect(resolveTargetFromBases('test/mock/bar/lib/a.txt', target)).toEqual('dist/lib/a.txt')
+    expect(resolveTargetFromBases('test/mock/@org/a.txt', target)).toEqual('dist/a.txt')
+    expect(resolveTargetFromBases('test/mock/@org/lib/a.txt', target)).toEqual('dist/lib/a.txt')
+  })
+})
