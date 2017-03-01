13c57b721cc1c8a0274fd25e2321cd3d810d6baf
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-03-01 22:08:23 +0100

    refactord naming

--- src/lib/resolve-target.js
+++ src/lib/resolve-target.js
@@ -2,14 +2,14 @@ import path from 'path'
 
 const cwd = process.cwd()
 
-const getBase = (file, bases) => bases.filter(base => file.indexOf(base) !== -1)
+const getBase = (source, bases) => bases.filter(base => source.indexOf(base) !== -1)
 // eslint-disable-next-line no-confusing-arrow
   .reduce((hit, base) => hit.length > base.length ? hit : base, '')
 
-const resolveTarget = bases => (file, target) => {
-  const from = path.join(cwd, getBase(file, bases))
+const resolveTarget = bases => (source, target) => {
+  const from = path.join(cwd, getBase(source, bases))
 
-  return path.join(target, path.relative(from, file))
+  return path.join(target, path.relative(from, source))
 }
 
 export default resolveTarget
