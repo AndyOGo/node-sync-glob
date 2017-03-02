5df652bf5ef6200a47744593eca856ad7be482ff
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-03-02 11:11:53 +0100

    ensureDir before copy a file

--- src/lib/fs.js
+++ src/lib/fs.js
@@ -5,7 +5,6 @@ import promisify from './promisify'
 
 const copy = promisify(fs.copy)
 const ensureDir = promisify(fs.ensureDir)
-const mkdir = promisify(fs.mkdir)
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 
@@ -25,10 +24,10 @@ export const copyFile = (source, target, transform) => new Promise((resolve) => 
       const data = (isObject && transformed.data) || transformed
       const newTarget = (isObject && transformed.target) || target
 
-      return mkdir(path.dirname(newTarget)).then(() => writeFile(newTarget, data))
+      return ensureDir(path.dirname(newTarget)).then(() => writeFile(newTarget, data))
     }))
   } else {
-    resolve(copy(source, target))
+    resolve(ensureDir(path.dirname(target)).then(() => copy(source, target)))
   }
 })
 
