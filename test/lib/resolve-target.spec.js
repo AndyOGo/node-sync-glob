2c87f9a7312d6c8df846a350a8b00012c6f636a0
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-04-06 01:14:58 +0200

    try to make spec cross os compliant

--- test/lib/resolve-target.spec.js
+++ test/lib/resolve-target.spec.js
@@ -1,3 +1,5 @@
+import path from 'path'
+
 import resolveTarget from '../../src/lib/resolve-target'
 
 describe('lib/resolve-target', () => {
@@ -12,15 +14,15 @@ describe('lib/resolve-target', () => {
     const resolveTargetFromBases = resolveTarget(bases)
     const target = 'dist'
 
-    expect(resolveTargetFromBases('test/a.txt', target)).toEqual('dist/a.txt')
-    expect(resolveTargetFromBases('test/lib/a.txt', target)).toEqual('dist/lib/a.txt')
-    expect(resolveTargetFromBases('test/mock/a.txt', target)).toEqual('dist/a.txt')
-    expect(resolveTargetFromBases('test/mock/lib/a.txt', target)).toEqual('dist/lib/a.txt')
-    expect(resolveTargetFromBases('test/mock/foo/a.txt', target)).toEqual('dist/a.txt')
-    expect(resolveTargetFromBases('test/mock/foo/lib/a.txt', target)).toEqual('dist/lib/a.txt')
-    expect(resolveTargetFromBases('test/mock/bar/a.txt', target)).toEqual('dist/a.txt')
-    expect(resolveTargetFromBases('test/mock/bar/lib/a.txt', target)).toEqual('dist/lib/a.txt')
-    expect(resolveTargetFromBases('test/mock/@org/a.txt', target)).toEqual('dist/a.txt')
-    expect(resolveTargetFromBases('test/mock/@org/lib/a.txt', target)).toEqual('dist/lib/a.txt')
+    expect(resolveTargetFromBases('test/a.txt', target)).toEqual(path.normalize('dist/a.txt'))
+    expect(resolveTargetFromBases('test/lib/a.txt', target)).toEqual(path.normalize('dist/lib/a.txt'))
+    expect(resolveTargetFromBases('test/mock/a.txt', target)).toEqual(path.normalize('dist/a.txt'))
+    expect(resolveTargetFromBases('test/mock/lib/a.txt', target)).toEqual(path.normalize('dist/lib/a.txt'))
+    expect(resolveTargetFromBases('test/mock/foo/a.txt', target)).toEqual(path.normalize('dist/a.txt'))
+    expect(resolveTargetFromBases('test/mock/foo/lib/a.txt', target)).toEqual(path.normalize('dist/lib/a.txt'))
+    expect(resolveTargetFromBases('test/mock/bar/a.txt', target)).toEqual(path.normalize('dist/a.txt'))
+    expect(resolveTargetFromBases('test/mock/bar/lib/a.txt', target)).toEqual(path.normalize('dist/lib/a.txt'))
+    expect(resolveTargetFromBases('test/mock/@org/a.txt', target)).toEqual(path.normalize('dist/a.txt'))
+    expect(resolveTargetFromBases('test/mock/@org/lib/a.txt', target)).toEqual(path.normalize('dist/lib/a.txt'))
   })
 })
